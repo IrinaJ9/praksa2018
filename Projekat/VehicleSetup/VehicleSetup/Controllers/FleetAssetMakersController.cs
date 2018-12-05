8f936367488be137054739a4e498f0a828439143
Branko <br.salabalija@gmail.com>
2018-12-06 00:04:55 +0100

    CRUD Maker i Model
    Napravljen CRUD za FleetAssetModel i FleetAssetMaker

---
+++ Projekat/VehicleSetup/VehicleSetup/Controllers/FleetAssetMakersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VehicleSetup.Models;
+
+namespace VehicleSetup.Controllers
+{
+    public class FleetAssetMakersController : Controller
+    {
+        private VehicleSetupEntities db = new VehicleSetupEntities();
+
+        // GET: FleetAssetMakers
+        public ActionResult Index()
+        {
+            return View(db.FleetAssetMake.ToList());
+        }
+
+        // GET: FleetAssetMakers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetMake fleetAssetMake = db.FleetAssetMake.Find(id);
+            if (fleetAssetMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fleetAssetMake);
+        }
+
+        // GET: FleetAssetMakers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FleetAssetMakers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Manufacturer")] FleetAssetMake fleetAssetMake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FleetAssetMake.Add(fleetAssetMake);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fleetAssetMake);
+        }
+
+        // GET: FleetAssetMakers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetMake fleetAssetMake = db.FleetAssetMake.Find(id);
+            if (fleetAssetMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fleetAssetMake);
+        }
+
+        // POST: FleetAssetMakers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Manufacturer")] FleetAssetMake fleetAssetMake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fleetAssetMake).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fleetAssetMake);
+        }
+
+        // GET: FleetAssetMakers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetMake fleetAssetMake = db.FleetAssetMake.Find(id);
+            if (fleetAssetMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fleetAssetMake);
+        }
+
+        // POST: FleetAssetMakers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FleetAssetMake fleetAssetMake = db.FleetAssetMake.Find(id);
+            db.FleetAssetMake.Remove(fleetAssetMake);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Projekat/VehicleSetup/VehicleSetup/Controllers/FleetAssetModelsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VehicleSetup.Models;
+
+namespace VehicleSetup.Controllers
+{
+    public class FleetAssetModelsController : Controller
+    {
+        private VehicleSetupEntities db = new VehicleSetupEntities();
+
+        // GET: FleetAssetModels
+        public ActionResult Index()
+        {
+            var fleetAssetModel = db.FleetAssetModel.Include(f => f.FleetAssetMake);
+            return View(fleetAssetModel.ToList());
+        }
+
+        // GET: FleetAssetModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetModel fleetAssetModel = db.FleetAssetModel.Find(id);
+            if (fleetAssetModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fleetAssetModel);
+        }
+
+        // GET: FleetAssetModels/Create
+        public ActionResult Create()
+        {
+            ViewBag.FleetAssetMakeID = new SelectList(db.FleetAssetMake, "ID", "Manufacturer");
+            return View();
+        }
+
+        // POST: FleetAssetModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,FleetAssetMakeID")] FleetAssetModel fleetAssetModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FleetAssetModel.Add(fleetAssetModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FleetAssetMakeID = new SelectList(db.FleetAssetMake, "ID", "Manufacturer", fleetAssetModel.FleetAssetMakeID);
+            return View(fleetAssetModel);
+        }
+
+        // GET: FleetAssetModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetModel fleetAssetModel = db.FleetAssetModel.Find(id);
+            if (fleetAssetModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FleetAssetMakeID = new SelectList(db.FleetAssetMake, "ID", "Manufacturer", fleetAssetModel.FleetAssetMakeID);
+            return View(fleetAssetModel);
+        }
+
+        // POST: FleetAssetModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,FleetAssetMakeID")] FleetAssetModel fleetAssetModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fleetAssetModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FleetAssetMakeID = new SelectList(db.FleetAssetMake, "ID", "Manufacturer", fleetAssetModel.FleetAssetMakeID);
+            return View(fleetAssetModel);
+        }
+
+        // GET: FleetAssetModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FleetAssetModel fleetAssetModel = db.FleetAssetModel.Find(id);
+            if (fleetAssetModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fleetAssetModel);
+        }
+
+        // POST: FleetAssetModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FleetAssetModel fleetAssetModel = db.FleetAssetModel.Find(id);
+            db.FleetAssetModel.Remove(fleetAssetModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
