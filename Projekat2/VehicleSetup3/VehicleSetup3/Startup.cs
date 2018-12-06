f18570a6589fa111ed59977d3620a53f954823b7
Unknown <irina.jez.9@hotmail.com>
2018-12-06 11:18:58 +0100

    Zapocet novi
    Uvezena baza

---
+++ Projekat2/VehicleSetup3/VehicleSetup3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VehicleSetup3.Startup))]
+namespace VehicleSetup3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
