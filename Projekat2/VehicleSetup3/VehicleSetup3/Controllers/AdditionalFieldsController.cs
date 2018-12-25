47d957c41f1a7c02a61a86607a7f6da27e4a2135
Unknown <irina.jez.9@hotmail.com>
2018-12-25 11:06:10 +0100

    Action u JsonResult

--- Projekat2/VehicleSetup3/VehicleSetup3/Controllers/AdditionalFieldsController.cs
+++ Projekat2/VehicleSetup3/VehicleSetup3/Controllers/AdditionalFieldsController.cs
@@ -81,16 +81,17 @@ namespace VehicleSetup3.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FleetNo,Name,Value")] AdditionalField additionalField)
+        public JsonResult Edit([Bind(Include = "ID,FleetNo,Name,Value")] AdditionalField additionalField)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(additionalField).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+               // return RedirectToAction("Index");
             }
             ViewBag.FleetNo = new SelectList(db.FleetAssets, "FleetNo", "RegistrationNo", additionalField.FleetNo);
-            return View(additionalField);
+            // return View(additionalField);
+            return Json(additionalField);
         }
 
         // GET: AdditionalFields/Delete/5
